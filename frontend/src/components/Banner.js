f5f8cbb3dd69fb260263e0fa1ae7a6f2589f398e
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-09-04 17:21:49 +0800

    change in col

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <section className='col-span-1 sm:col-span-12 text-center'>
+    <section className='col-span-12 text-center'>
       <div className='font-bold text-3xl md:text-4xl'>Our Monthly Blogs</div>
       <div className='mt-2 text-sm md:text-base'>Elevate your research writing skills today.</div>
     </section>
