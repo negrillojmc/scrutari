a9e7a240991eaf4f6802d4570aa2eed15bf2a7b7
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-09-04 17:22:40 +0800

    Insertion of the "Feed" component; temp removal of the "Searchbar" component

--- frontend/src/pages/Blog.js
+++ frontend/src/pages/Blog.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Banner from '../components/Banner'
-import SearchBar from '../components/SearchBar'
+// import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
+import Feed from './blog/Feed'
 
 const Blog = () => {
   return (
     <div>
-      <main className='grid grid-cols-1 sm:grid-cols-12 p-10 sm:px-20 md:px-28 lg:px-44 py-10 sm:gap-x-4 xl:mx-32'>
+      <main className='grid grid-cols-1 lg:grid-cols-3 p-10 sm:px-20 md:px-28 py-10 sm:gap-x-4 xl:mx-32'>
         <Banner />
-        <SearchBar />
+        {/* <SearchBar /> */}
         <Categories />
-
-        
+        <Feed />
       </main>
     </div>
   )
