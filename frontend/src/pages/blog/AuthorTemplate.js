ff010e4b977de03e3e167804bea7075e7604d851
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-11-16 07:30:17 +0800

    attempt to fetch "author" and "article" dbs

--- frontend/src/pages/blog/AuthorTemplate.js
+++ frontend/src/pages/blog/AuthorTemplate.js
@@ -6,18 +6,30 @@ import { BiLogoGmail } from "react-icons/bi";
 const AuthorTemplate = () => {
   const { id } = useParams();
   const [article, setArticle] = useState(null);
+  const [author, setAuthor] = useState(null);
 
   useEffect(() => {
     const fetchArticle = async () => {
-      const response = await fetch(`http://localhost:2000/api/blog/${id}`);
-      const json = await response.json();
+      const articleResponse = await fetch(`http://localhost:2000/api/blog/${id}`);
+      const articleJSON = await articleResponse.json();
 
-      if (response.ok) {
-        setArticle(json);
+      if (articleResponse.ok) {
+        setArticle(articleJSON);
       }
     };
 
     fetchArticle();
+
+    const fetchAuthor = async () => {
+      const authorResponse = await fetch (`http://localhost:2000/api/author/${id}`);
+      const authorJSON = await authorResponse.json();
+
+      if (authorResponse.ok) {
+        setAuthor(authorJSON);
+      }
+    };
+
+    fetchAuthor();
   }, [id]);
 
   return (
@@ -25,8 +37,8 @@ const AuthorTemplate = () => {
       {article && (
         <div className="bg-gray-100 flex container flex-col items-center justify-center p-4 rounded-2xl h-auto xl:h-1/4">
           <img
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="x"
+            src={author.image}
+            alt={author.name}
             className="aspect-ratio-1/1 rounded-full w-32 h-32 object-cover mb-2"
           />
           <h1 className="font-bold">{article.author}</h1>
@@ -34,7 +46,7 @@ const AuthorTemplate = () => {
             <FaFacebook className="h-5 w-5" />
             <BiLogoGmail className="h-5 w-5 ml-3" />
           </div>
-          <p className="p-4 text-sm text-center">{article.content}</p>
+          <p className="p-4 text-sm text-center">{author.bio}</p>
         </div>
       )}
     </div>
