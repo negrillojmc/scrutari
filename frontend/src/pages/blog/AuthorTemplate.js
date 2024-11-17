d8bdb2b28b0fd68962e718c5abfadc0c9d6c16ac
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-11-18 03:05:08 +0800

    fixed useEffect

--- frontend/src/pages/blog/AuthorTemplate.js
+++ frontend/src/pages/blog/AuthorTemplate.js
@@ -5,40 +5,26 @@ import { BiLogoGmail } from "react-icons/bi";
 
 const AuthorTemplate = () => {
   const { id } = useParams();
-  const [article, setArticle] = useState(null);
-  const [author, setAuthor] = useState(null);
+  const [article, setArticle] = useState(null); // set the article
+  // const [author, setAuthor] = useState(null); // set the object that has the matching authorID
 
   useEffect(() => {
     const fetchArticle = async () => {
-      const articleResponse = await fetch(`http://localhost:2000/api/blog/${id}`);
-      const articleJSON = await articleResponse.json();
+      const response = await fetch(`http://localhost:2000/api/blog/${id}`);
+      const data = await response.json();
+      setArticle(data);
+    }
 
-      if (articleResponse.ok) {
-        setArticle(articleJSON);
-      }
-    };
-
-    fetchArticle();
-
-    const fetchAuthor = async () => {
-      const authorResponse = await fetch (`http://localhost:2000/api/author/${id}`);
-      const authorJSON = await authorResponse.json();
-
-      if (authorResponse.ok) {
-        setAuthor(authorJSON);
-      }
-    };
-
-    fetchAuthor();
-  }, [id]);
+    fetchArticle()
+  }, [id])
 
   return (
     <div>
       {article && (
         <div className="bg-gray-100 flex container flex-col items-center justify-center p-4 rounded-2xl h-auto xl:h-1/4">
           <img
-            src={author.image}
-            alt={author.name}
+            src={article.authorID.image}
+            alt={article.name}
             className="aspect-ratio-1/1 rounded-full w-32 h-32 object-cover mb-2"
           />
           <h1 className="font-bold">{article.author}</h1>
@@ -46,7 +32,7 @@ const AuthorTemplate = () => {
             <FaFacebook className="h-5 w-5" />
             <BiLogoGmail className="h-5 w-5 ml-3" />
           </div>
-          <p className="p-4 text-sm text-center">{author.bio}</p>
+          <p className="p-4 text-sm text-center">{article.authorID.bio}</p>
         </div>
       )}
     </div>
