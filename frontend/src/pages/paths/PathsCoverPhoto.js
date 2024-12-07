121ce4e35c07014fc130e1f6d75c8b7eee1fc422
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-12-07 09:56:33 +0800

    added cover photo on Paths page

---
+++ frontend/src/pages/paths/PathsCoverPhoto.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CoverPic from '../../components/CoverPic'
+
+const PathsCoverPhoto = () => {
+  return (
+    <div>
+        <CoverPic text="Paths" imageSrc="https://tinyurl.com/r4635tk2" altText="Paths page cover photo" />
+    </div>
+  )
+}
+
+export default PathsCoverPhoto
