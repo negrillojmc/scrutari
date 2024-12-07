05fce993a1c24efdc6a8f4323e7880640a1d04c6
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-12-07 09:55:56 +0800

    removed color presets

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {
-        introduction: '#06d068', // Benzol Green
-        literature: '#07d6c1', // Pristine Oceanic
-        methodology: '#14b0f7', // Zima Blue
-        analysis: '#3d81ff' // Dodger Blue
-      }
+      colors: {},
     },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animate")],
+};
