31018a8ab08a0e7e554ee7dfe850ba7c732c532f
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2024-12-07 09:53:03 +0800

    added Paths page

---
+++ frontend/src/pages/Paths.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PathsCoverPhoto from './paths/PathsCoverPhoto'
+import HeadingsCategories from './paths/HeadingsCategories'
+import ResearchCategories from './paths/ResearchCategories'
+import HeadingsImpact from './paths/HeadingsImpact'
+import ResearchAreas from './paths/ResearchAreas'
+import FAQs from './paths/FAQs'
+
+
+const Paths = () => {
+  return (
+    <div>
+        <PathsCoverPhoto />
+        <HeadingsCategories />
+        <ResearchCategories />
+        <HeadingsImpact />
+        <ResearchAreas />
+        <FAQs />
+    </div>
+  )
+}
+
+export default Paths
