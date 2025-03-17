3e2643d71eb6d8ccc59dcc75e4d82ddc9de620e5
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2025-03-18 04:03:30 +0800

    Added a reusable Button component

--- frontend/src/components/ButtonGradient.js
+++ frontend/src/components/ButtonGradient.js
@@ -4,7 +4,7 @@ import { MdArrowOutward } from "react-icons/md";
 const ButtonGradient = ({text}) => {
   return (
     <div>
-      <button className="mt-6 py-2 px-6 bg-gradient-to-r from-[#1CB5E0] to-[#000046] rounded-3xl w-full flex gap-2 items-center justify-center font-semibold">
+      <button className="py-2 px-6 bg-gradient-to-r from-[#1CB5E0] to-[#000046] rounded-3xl w-full flex gap-2 items-center justify-center font-semibold">
         <p className="text-white text-sm">{text}</p>
         <MdArrowOutward className="text-white size-4" />
       </button>
