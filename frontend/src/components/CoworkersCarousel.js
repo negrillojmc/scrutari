0f49b3be3f7aab9f55b7a01e6fbb063d6463f307
chromatid10 <136996895+chromatid10@users.noreply.github.com>
2025-03-18 04:03:59 +0800

    Created Coworkers carousel

---
+++ frontend/src/components/CoworkersCarousel.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+// https://tinyurl.com/3f476jrd
+
+const CoworkersCarousel = () => {
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+  return (
+    <div className="">
+
+    </div>
+  );
+};
+
+export default CoworkersCarousel;
